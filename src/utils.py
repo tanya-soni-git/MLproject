7e3d2499a70cde0dbdc3e9ad1c69086e49f11723
Tanya Soni <tanya25soni@gmail.com>
2025-07-02 23:57:16 +0530

    application

--- src/utils.py
+++ src/utils.py
@@ -10,6 +10,13 @@ from src.exception import CustomException
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
+
 def save_object(file_path,obj):
     try:
         dir_path=os.path.dirname(file_path)
